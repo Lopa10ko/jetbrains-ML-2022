5c3a48a80dfa822ca378bfec663304fcd61f4b33
Lopa10ko G <81328772+Lopa10ko@users.noreply.github.com>
2022-07-08 10:09:20 +0300

    Update log_reg2.py

--- Logistic_regression/stage2/log_reg2.py
+++ Logistic_regression/stage2/log_reg2.py
@@ -30,6 +30,18 @@ class CustomLogisticRegression:
                 for j in range(row.shape[1]):
                     self.coef_[j] -= self.l_rate * (y_hat - y_train.values[i]) * y_hat * (1 - y_hat) * row[0, j]
 
+    def fit_log_loss(self, X_train, y_train):
+        self.coef_ = np.zeros(self.fit_intercept + X_train.shape[1])  # initialized weights
+        for _ in range(self.n_epoch):
+            for i, row in enumerate(X_train.values):
+                row = row.tolist()
+                if self.fit_intercept:
+                    row.insert(0, 1)
+                row = np.array([row])
+                y_hat = self.predict_proba(row, self.coef_)
+                for j in range(row.shape[1]):
+                    self.coef_[j] -= self.l_rate * (y_hat - y_train.values[i]) * row[0, j] / (self.fit_intercept + X_train.shape[0])
+
     def predict(self, X_test, cut_off=0.5):
         predictions = np.zeros(X_test.shape[0], dtype=int)
         for i, row in enumerate(X_test.values):
@@ -46,5 +58,7 @@ X, y = datasets.load_breast_cancer(return_X_y=True, as_frame=True)
 X = X[['worst concave points', 'worst perimeter', 'worst radius']].apply(zscore)
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=43)
 my_logreq = CustomLogisticRegression()
-my_logreq.fit_mse(X_train, y_train)
+# my_logreq.fit_mse(X_train, y_train)
+my_logreq.fit_log_loss(X_train, y_train)
 print({'coef_': my_logreq.coef_.tolist(), 'accuracy': np.sum(my_logreq.predict(X_test) == y_test) / X_test.shape[0]})
+# print({'coef_': my_logreq.coef_.tolist(), 'accuracy': (-1 / X_test.shape[0]) * })
