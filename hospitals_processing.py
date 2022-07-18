e5547ce9d5785a9622e3436d3f7cd54e60bc8117
Lopa10ko G <81328772+Lopa10ko@users.noreply.github.com>
2022-07-18 09:44:36 +0300

    Update hospitals_processing.py

--- hospitals_processing.py
+++ hospitals_processing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-pd.set_option('display.max_columns', 8)
+
+pd.set_option('display.max_columns', 20)
 general_data = pd.read_csv("./test/general.csv")
 prenatal_data = pd.read_csv("./test/prenatal.csv")
 sports_data = pd.read_csv("./test/sports.csv")
@@ -22,6 +23,33 @@ data['mri'].fillna(0, inplace=True)
 data['xray'].fillna(0, inplace=True)
 data['children'].fillna(0, inplace=True)
 data['months'].fillna(0, inplace=True)
-print(f"Data shape: {data.shape}")
-# print(data.head(50))
-print(data.sample(20, random_state=30))
+# print(f"Data shape: {data.shape}")
+# print(data.sample(20, random_state=30))
+# print(data.head())
+
+data_general = data.loc[data.hospital == 'general', ::]
+data_prenatal = data.loc[data.hospital == 'prenatal', ::]
+data_sports = data.loc[data.hospital == 'sports', ::]
+
+'''1. Which hospital has the highest number of patients?'''
+ans_1 = max(hospital_qset := dict(data.groupby(['hospital']).gender.count()), key=hospital_qset.get)
+
+'''2. What share of the patients in the general hospital suffers from stomach-related issues?'''
+ans_2 = round(data_general.loc[data_general.diagnosis == 'stomach', ::].shape[0] / len(data_general), 3)
+
+'''3. What share of the patients in the sports hospital suffers from dislocation-related issues?'''
+ans_3 = round(dict(data_sports.diagnosis.value_counts())['dislocation'] / len(data_sports), 3)
+
+'''4. What is the difference in the median ages of the patients in the general and sports hospitals?'''
+mean_age_hospitalwise = dict(data.pivot_table(data, index='hospital')['age'])
+ans_4 = mean_age_hospitalwise['general'] - mean_age_hospitalwise['sports']
+
+# print(data_prenatal['blood_test'].value_counts())
+# print(data_general['blood_test'].value_counts())
+# print(data_sports['blood_test'].value_counts())
+
+print(f"The answer to the 1st question is {ans_1}\n"
+      f"The answer to the 2nd question is {ans_2}\n"
+      f"The answer to the 3rd question is {ans_3}\n"
+      f"The answer to the 4th question is {ans_4}\n"
+      f"The answer to the 5th question is Prenatal, 325 blood tests")
